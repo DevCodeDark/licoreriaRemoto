9d9412f668fb2e270e0da73c159166450e041306
Warning <acunarenato4@gmail.com>
2025-06-09 07:53:56 -0500

    Categorias

--- src/main/java/com/sipsoft/licoreria/controller/CategoriaController.java
+++ src/main/java/com/sipsoft/licoreria/controller/CategoriaController.java
@@ -13,27 +13,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sipsoft.licoreria.dto.CategoriaDTO;
 import com.sipsoft.licoreria.entity.Categoria;
+import com.sipsoft.licoreria.entity.Empresa;
+import com.sipsoft.licoreria.repository.EmpresaRepository;
 import com.sipsoft.licoreria.services.ICategoriaService;
 
 @RestController
 @RequestMapping("/sipsoft")
+
 public class CategoriaController {
     @Autowired
     private ICategoriaService serviceCategoria;
+
+    @Autowired
+    private EmpresaRepository repoEmpresa;
 
     @GetMapping("/categorias")
     public List<Categoria> buscarTodos() {
         return serviceCategoria.bucarTodos();
     }
+
     @PostMapping("/categorias")
-    public Categoria guardar(@RequestBody Categoria categoria) {
+    public Categoria guardar(@RequestBody CategoriaDTO dto) {
+        Categoria categoria = new Categoria();
+        categoria.setNombreCategoria(dto.getNombreCategoria());
+        categoria.setEstadoCategoria(dto.getEstadoCategoria());
+        Empresa empresa = repoEmpresa.findById(dto.getIdEmpresa()).orElse(null);
+        categoria.setIdEmpresa(empresa);
         serviceCategoria.guardar(categoria);
         return categoria;
     }
 
     @PutMapping("/categorias")
-    public Categoria modificar(@RequestBody Categoria categoria) {
+    public Categoria modificar(@RequestBody CategoriaDTO dto) {
+        Categoria categoria = new Categoria();
+        categoria.setIdCategoria(dto.getIdCategoria());
+        categoria.setNombreCategoria(dto.getNombreCategoria());
+        categoria.setEstadoCategoria(dto.getEstadoCategoria());
+        Empresa empresa = repoEmpresa.findById(dto.getIdEmpresa()).orElse(null);
+        categoria.setIdEmpresa(empresa);
         serviceCategoria.modificar(categoria);
         return categoria;
     }
